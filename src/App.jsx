b00429601a5e352499e074dd70f8925dbd31ae5b
JuanOrtizLetrado <jumortizl@correo.udistrital.edu.co>
2021-10-17 18:27:18 -0500

    configuracion de los import con el jsconfig

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import Index from 'pages';
+import 'styles/style.css';
+
+
+
+
+function App() {
+  return (
+    <div className='App'>
+      <Index/>
+    </div>
+    
+  );
+}
+
+export default App;
